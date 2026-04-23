cd2ea7fb356e3939c6bd74a4b89f307b42af90f4
RVSJAYANTH <rvsjayanth999@gmail.com>
2026-04-23 23:41:35 +0530

    Mongodb_Update

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB=async()=>{
     try{
         mongoose.connection.on("connected",()=>console.log("Database connected"));
-        await mongoose.connect(`${process.env.MONGO_URI}/greenmart`);
+        await mongoose.connect(`${process.env.MONGO_URI}/test`);
     }catch(error){
         console.error(error.message);
     }
